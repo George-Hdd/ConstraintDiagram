be7cae249ab43b7e32a0425a52b9825053cb10df
PranavaaKirupakaran <pranavaa.kirupakaran@gmail.com>
2022-11-01 19:46:33 -0400

    added all parameters that needs to be used

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-def
+import pandas as pandaBoi
+
+rho_0 = 0 # air density 0
+Vs = 0 # stall speed
+Cl_max = 0 # Max Lift Coefficient
+Vmax = 0 # Max. Speed
+Cd_0 = 0 # Zero-lift drag coeffcient
+e = 0 # Oswald span efficiency
+AR = 0 # Aspect Ratio
+K = 0 # induced drag factor
+rho_usual = 0 # air density usual
+sigma_usual = 0 # sigma absolute ceiling (rho_usual/rho_0)
+np = 0 # propeller efficiency
+V_thrust = 0 # Engines Thrust
+V_wod = 0 # wind over deck
+V_end = 0 # end catapult speed
+ROC = 0 # Rate of climb
+LD_Max = 0 # Max lift to drag ratio
+rho_roc = 0 # air density rate of climb
+rho_ac = 0 # air density at absolute ceiling
+sigma_ac = 0 # sigma absolute ceiling
+
+def updateParameters():
+    
 
 if __name__ == '__main__':
     print("Hello, George!")
